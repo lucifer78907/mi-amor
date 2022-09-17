0505bcdafe7ab6b285597c2c256854f8ddd3f3a6
Rudra Pratap Singh <chaudhary66667@gmail.com>
2022-09-17 17:47:33 +0530

    +features_mi-amor

---
+++ js/scroll.js
@@ -0,0 +1,6 @@
+const featureSection = document.querySelector('.features');
+const header = document.querySelector('.header');
+
+// document.addEventListener('scroll', function () {
+//   featureSection.scrollIntoView({ behavior: 'smooth' });
+// });
