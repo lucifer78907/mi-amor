0ad7f448cf9a529353b9c02181d80c55f241007e
Rudra Pratap Singh <chaudhary66667@gmail.com>
2022-09-17 22:45:43 +0530

    +why_chose_us

--- js/testimonial.js
+++ js/testimonial.js
@@ -1,13 +1,14 @@
 class testimonial {
   #btnLeft = document.querySelector('.circle__btn--left');
   #btnRight = document.querySelector('.circle__btn--right');
-  #namesArr = ['lisa williams', 'jayed wolf', 'lil lover'];
+  #namesArr = ['~ lisa williams', '~ jayed wolf', '~ lil lover'];
   #descArr = [
     ' This application was a gamechanger for me , previously i was very very scared to go out and ask out people but after using mi-amor i found myself a lovely person who accepts me the way i am!  ',
     ' Wow , i am amazed by mi-amor, previously i was it very hard to go out and ask out people due to my shy nature, now i can easily go around on the application and meet the love of my life!.',
     ' Bravo!, mi-amor provides a healthy community to contact with and go on finding people with same interests , i found my love interest without any harrasment and bullying , that is heaven to me  ',
   ];
   #designationArr = ['tennis player', 'dentist', 'construction site worker'];
+  #testimonialBox = document.querySelector('.testimonial__box');
   #testimonialImg = document.querySelector('.testimonial__img');
   #testimonialPara = document.querySelector('.testimonial__para');
   #testimonialName = document.querySelector('.testimonial__author');
@@ -24,7 +25,6 @@ class testimonial {
   }
 
   _showPrev() {
-    console.log(this._currSlide);
     this._currSlide -= 1;
     if (this._currSlide <= 0) this._currSlide = 3;
 
