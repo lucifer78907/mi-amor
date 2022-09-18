1ca65c71d32f79742409db2f5b632ac6d560f8cd
Rudra Pratap Singh <chaudhary66667@gmail.com>
2022-09-18 09:07:00 +0530

    +footer icons

--- js/controller.js
+++ js/controller.js
@@ -1,1 +1,2 @@
 import testimonial from './testimonial.js';
+import scroll from './scroll.js';
--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,29 @@
-const featureSection = document.querySelector('.features');
-const header = document.querySelector('.header');
+class scroll {
+  #nav = document.querySelector('.nav');
+  #sections = document.querySelectorAll('.section');
+  #footer = document.querySelector('.footer');
+  #allSections = [this.#nav, ...this.#sections, this.#footer];
+  #currIndx = 0;
 
-// document.addEventListener('scroll', function () {
-//   featureSection.scrollIntoView({ behavior: 'smooth' });
-// });
+  constructor() {
+    this._addEventListner();
+  }
+
+  _addEventListner() {
+    document.addEventListener('wheel', this._scroll.bind(this));
+  }
+
+  _scroll(eve) {
+    if (eve.deltaY > 0) {
+      this._scrollIntoView();
+    }
+  }
+
+  _scrollIntoView() {
+    this.#currIndx += 1;
+    if (this.#currIndx > 4) this.#currIndx = 0;
+    this.#allSections[this.#currIndx].scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+export default new scroll();
